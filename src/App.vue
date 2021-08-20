f9e94e282481412e7355df4f95e4824b2cd7feb5
Davaria <Daprimovaria@gmail.com>
2021-08-20 18:07:05 -0500

    chore: update css

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,14 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Special Elite', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30vh;
 }
 </style>
